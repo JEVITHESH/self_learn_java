67374be8bb94eafc342495dc8fd1f597ee7b06d6
JEVITHESH <151340536+JEVITHESH@users.noreply.github.com>
2025-07-10 12:00:38 +0530

    Create array sort.java

---
+++ array sort.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+public class sub {
+     public static void main(String[] args) {
+        String[] food = {"apple","orange","banana"};
+        Arrays.sort(food);
+        for(String foods : food){
+         System.out.println(foods);
+        }
+     }}
+
+
