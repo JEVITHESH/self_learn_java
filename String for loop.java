c0739ab553e4aeac3b961f2378c4707fafe188fb
JEVITHESH <151340536+JEVITHESH@users.noreply.github.com>
2025-07-10 11:52:31 +0530

    Create String for loop.java

---
+++ String for loop.java
@@ -0,0 +1,10 @@
+public class sub {
+     public static void main(String[] args) {
+        String[] food = {"apple","orange","banana"};
+        int tol = food.length;        
+        for (int i = 0; i < tol;i++){
+         System.out.println(food[i]+" "); 
+          
+        }     
+     }
+}
