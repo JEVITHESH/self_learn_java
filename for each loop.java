5b2db89844791fc8f33ea80dcf1f9bb396fd56bd
JEVITHESH <151340536+JEVITHESH@users.noreply.github.com>
2025-07-10 11:54:10 +0530

    Create for each loop.java

---
+++ for each loop.java
@@ -0,0 +1,10 @@
+public class sub {
+     public static void main(String[] args) {
+        String[] food = {"apple","orange","banana"};
+        
+        
+        for (String foods : food){
+         System.out.println(foods+" ");
+         
+          
+        }
