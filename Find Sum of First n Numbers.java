dd04ed9091f55d183fcdd6ba3a2f339d1bda2d70
JEVITHESH <151340536+JEVITHESH@users.noreply.github.com>
2025-07-26 12:10:34 +0530

    Create Find Sum of First n Numbers.java

---
+++ Find Sum of First n Numbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class sub{
+  public static void main(String[] args){
+  System.out.print("Enter the n number : ");
+  Scanner sc = new Scanner(System.in);
+  int n = sc.nextInt();
+  int a=(n*(n+1)/2);
+  System.out.println(a);
+  }
+  
+  
+}
