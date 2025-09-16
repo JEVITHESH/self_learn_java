dcf2adf61d444ba3845b3b33775e07ba7a73c976
JEVITHESH <151340536+JEVITHESH@users.noreply.github.com>
2025-09-16 12:03:48 +0530

    Create leap_year.java

---
+++ leap_year.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+    
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int a = sc.nextInt();
+      if(a%4==0){
+          System.out.print("it is leap year");
+      }
+      else{
+           System.out.print("it is not leap year");
+      
+      }
+       
+       
+} }
