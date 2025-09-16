449371bbf7ecdd2d299f0ebbd479aa71c9eaba7e
JEVITHESH <151340536+JEVITHESH@users.noreply.github.com>
2025-09-16 10:35:56 +0530

    Create Assignmenr_operators.java

---
+++ Assignmenr_operators.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.lang.Math;
+class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int c =5;
+       System.out.print("Enter the number-1: ");
+       int num1 = sc.nextInt();
+      // c +=num1;
+      //c *= num1;
+        c-=num1;
+       System.out.print(c);
+       
+} }
