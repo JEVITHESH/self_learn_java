dd976c2305c9d050342ff50209b2cd7ea51945be
JEVITHESH <151340536+JEVITHESH@users.noreply.github.com>
2025-09-30 09:57:45 +0530

    Create private.java

---
+++ oop/private.java
@@ -0,0 +1,13 @@
+class a{
+    private int num = 10;
+    void display(){
+        System.out.print(num);
+    }
+}
+public class Main
+{
+	public static void main(String[] args) {
+		a obj = new a();
+		obj.display();
+	}
+}
