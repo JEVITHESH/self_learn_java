2b299f498540aa3ae636c448c183d21f4ec54aee
JEVITHESH <151340536+JEVITHESH@users.noreply.github.com>
2025-09-30 10:00:40 +0530

    Create getter_private.java

---
+++ oop/getter_private.java
@@ -0,0 +1,20 @@
+class A {
+    private int num = 10;
+
+    int getNum() {       // Getter method
+        return num;
+    }
+}
+
+class B extends A {
+    void display(){
+        System.out.print(getNum()); // Access via getter
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        B obj = new B();
+        obj.display();
+    }
+}
