a4c499d2ef2ebb79ad68645da35a6481a0b52469
JEVITHESH <151340536+JEVITHESH@users.noreply.github.com>
2025-12-17 23:00:35 +0530

    Create Find the greatest number from 3 numbers.java

---
+++ Find the greatest number from 3 numbers.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int num1 = sc.nextInt();
+	int num2 = sc.nextInt();
+	int num3 = sc.nextInt();
+	if(num1<=num2 && num3<=num2){
+	    System.out.print(num2);
+	}
+	else if(num2<=num3 && num1<=num3){
+	    System.out.print(num3);
+	}
+	else{
+	    System.out.print(num1);
+	}
+	}
+}
