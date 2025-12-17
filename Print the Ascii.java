92b753f59cf3b9505bbe103e6f08f9a66b4f8603
JEVITHESH <151340536+JEVITHESH@users.noreply.github.com>
2025-12-18 00:34:16 +0530

    Create Print the Ascii.java

---
+++ Print the Ascii.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+	char num1 = sc.next().charAt(0);
+	int a = num1;
+    System.out.print(a);
+	}
+}
